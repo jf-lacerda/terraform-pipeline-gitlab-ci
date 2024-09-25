9e0974704ef829aec342bf193e14b3f944254990
João Lacerda <joao.sclf@gmail.com>
2024-09-25 10:14:54 -0300

    Adicionando configurações de VMs Azure e AWS

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "joaoterraformstate9"
+    storage_account_name = "joaoterraformstate96"
     container_name       = "remote-state"
     key                  = "pipeline-gitlab/terraform.tfstate"
   }
